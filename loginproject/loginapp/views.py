938b2b79088c53e160275b863864b7eb94fd3525
srinivasan70 <91547769+srinivasan70@users.noreply.github.com>
2021-10-13 10:07:37 +0530

    Add files via upload

--- loginproject/loginapp/views.py
+++ loginproject/loginapp/views.py
@@ -5,8 +5,67 @@ from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 import json
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+
+class post_sign(APIView):
+    @csrf_exempt
+           
+    def post(self,request):
+        try:
+            
+            request_data=request.data
+            print(request_data)
+        
+            username=request_data['data']['username']
+            email=request_data['data']['email']
+            password=request_data['data']['password']
+            first_name=request_data['data']['first_name']
+            last_name=request_data['data']['last_name']
+            confirm_password=request_data['data']['confirm_password']
+            print(username)
+            print(email)
+            print(password)
+            print(first_name)
+            print(last_name)
+            print(confirm_password)
+            
+            u=User.objects.get(username=username)
+            if u:
+                return Response(status =status.HTTP_400_BAD_REQUEST, data={"remarks": 'User Created already'})
+            u1=User.objects.get(email=email):
+            if u1:
+                return Response(status =status.HTTP_400_BAD_REQUEST, data={"remarks": 'Email Created already'})
+            if(password==confirm_password):
+                user = User.objects.create_user(username,email,password)
+                user.first_name=first_name
+                user.last_name=last_name
+                user.save()
+                return Response(status=status.HTTP_200_OK, data={"remarks":'User Created Successfully'})
+            else:
+                return Response(status =status.HTTP_400_BAD_REQUEST, data={"remarks": 'Password and Confirm Password do not match'})
+        except :
+            print("Error")
+            
+            
+            
+        
+        
+            
+        
+class post_login(APIView):
+    @csrf_exempt
+    def post_login(request):
+        '''try:'''
+        return Response(request.data )
+        '''except:
+            print("Error")'''
+        
+        
+# Create your views here.
+'''
 @csrf_exempt
 def post_login(request):
     request_json=json.loads(request.body)
@@ -37,3 +96,6 @@ def post_login(request):
 
 def get_login(request):
     return HttpResponse("Details")
+'''
+
+
